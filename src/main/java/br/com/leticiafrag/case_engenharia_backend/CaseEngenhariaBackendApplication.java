9fd2c7d4882ce8007f33af8235c03dbb8ff79076
Letícia França <canumsang@gmail.com>
2024-12-20 11:06:51 -0300

    initial commit: set up spring boot project with spring web dependency

---
+++ src/main/java/br/com/leticiafrag/case_engenharia_backend/CaseEngenhariaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.leticiafrag.case_engenharia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseEngenhariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseEngenhariaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leticiafrag/case_engenharia_backend/CaseEngenhariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leticiafrag.case_engenharia_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseEngenhariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
