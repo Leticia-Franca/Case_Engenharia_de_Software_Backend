cabd49ebb55df17815063ea2d5e4a7f3bb04d90c
Letícia França <canumsang@gmail.com>
2024-12-21 11:43:12 -0300

    feat: adds EMAIL_DUPLICATED in enum UserValidation

--- src/main/java/br/com/leticiafrag/case_engenharia_backend/domain/UserValidation.java
+++ src/main/java/br/com/leticiafrag/case_engenharia_backend/domain/UserValidation.java
@@ -6,8 +6,9 @@ public enum UserValidation {
     NAME_LENGTH(2, "ERROR: The name must have between 3 and 50 characters."),
     NAME_INVALID(3, "ERROR: The name must have only letters and spaces."),
     EMAIL_EMPTY(4, "ERROR: The email cannot be empty."),
-    EMAIL_INVALID(5, "ERROR: The informed email is invalid."),
-    AGE_INVALID(6, "ERROR: Age must be between 0 and 123.");
+    EMAIL_INVALID(5, "ERROR: The email is invalid."),
+    EMAIL_DUPLICATED(6, "ERROR: The email is already being used."),
+    AGE_INVALID(7, "ERROR: The age must be between 0 and 123.");
 
     private final int code;
     private final String message;
