7ffebe845a4a9d0624c974ef05078d16e831aabc
Letícia França <canumsang@gmail.com>
2024-12-21 17:32:56 -0300

    chore: adds comments to UserValidation enum explaining its use

--- src/main/java/br/com/leticiafrag/case_engenharia_backend/domain/UserValidation.java
+++ src/main/java/br/com/leticiafrag/case_engenharia_backend/domain/UserValidation.java
@@ -1,5 +1,15 @@
 package br.com.leticiafrag.case_engenharia_backend.domain;
 
+/*
+* Enum representing the validation results for a user.
+* Each value in this enum corresponds to a specific validation rule
+* applied to the user data (such as name, email, and age). The enum
+* includes an associated error code and message to describe the result of
+* each validation check.
+*
+* This way, the returns in the isUserValid method (implemented in UserService)
+* are more organized and clear.
+* */
 public enum UserValidation {
     VALID(0, "User is valid."),
     NAME_EMPTY(1, "ERROR: The name cannot be empty."),
